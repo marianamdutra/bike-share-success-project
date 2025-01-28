312021a57fdcd822ee0efb1f061614fac22038c5
marianamdutra <marianamd.8@gmail.com>
2025-01-27 22:12:19 -0300

    adding to-do

--- project-development/main.py
+++ project-development/main.py
@@ -11,5 +11,14 @@ for i in range(len(prefixes)):
     archives.append(directory+prefixes[i]+sufix)
 print(archives)
 
-with zip.ZipFile(directory, mode='r') as myzip:
+with zip.ZipFile(archives[0], mode='r') as myzip:
     myzip.printdir
+
+## Próximos passos: 
+
+# 1. Abrir os .zip e criar DataFrames pandas a partir dos .csv
+# 2. Criar colunas de "tempo de viagem" e "dia da semana"
+# 3. Verificar a integridade dos dados e documentar qualquer manipulação e limpeza feita.
+# 4. Organize e formate os dados.
+# 5. Realize cálculos descritivos, como médias e máximos de tempos de viagem.
+# 6. Crie tabelas dinâmicas para visualizar padrões, como a média de tempo de viagem por tipo de usuário.
